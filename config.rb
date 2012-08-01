6bf98e85dc5e02da098012676539a0418503e403
Tim Huegdon <tim@nefariousdesigns.co.uk>
2012-08-01 22:27:12 +0100

    First commit.

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "source/assets/css/"
+sass_dir = "sass"
+images_dir = "source/assets/img/"
+javascripts_dir = "source/assets/js/"
+
+output_style = :compressed
+
+relative_assets = true
