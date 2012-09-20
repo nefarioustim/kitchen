a83c095466450b476d5c89ff5520ca8d606ca19b
Tim Huegdon <tim@nefariousdesigns.co.uk>
2012-09-20 20:01:12 +0100

    Update config.

--- config.rb
+++ config.rb
@@ -1,9 +1,12 @@
+project_type = :stand_alone
+
 http_path = "/"
 css_dir = "source/assets/css/"
 sass_dir = "sass"
 images_dir = "source/assets/img/"
 javascripts_dir = "source/assets/js/"
 
+line_comments = false
 output_style = :compressed
 
 relative_assets = true
